439caf53f91e16f68f7de9c9eae6afc197d33717
Kwon Valerii <wellko24@gmail.com>
2023-07-12 14:45:42 +0600

    created axios request logic and useEffect for getting data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+
+interface Contributions {
+    [key: string]: string
+}
+const axiosApi = axios.create({
+    baseURL: ' https://dpg.gg/test/'
+});
 
 function App() {
-  return (
-    <div className="App">
+    const [contributions, setContributions] = useState<Contributions>({})
+    const getContributions = useCallback(async () => {
+        try {
+            const result = await axiosApi.get<Contributions>('calendar.json');
+            if (result.data !== null) {
+                setContributions(result.data);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }, []);
 
-    </div>
-  );
+    useEffect(() => {
+        getContributions().catch(console.error)
+    }, [getContributions])
+    return (
+        <div className="App">
+            <button onClick={() => console.log(contributions)}/>
+        </div>
+    );
 }
 
 export default App;
